28cb0e2e5d70ebdbb1e39c92ab0295e000b8ebab
Nicholas Ochs <nick.ochs@yahoo.com>
2020-10-15 20:48:08 -0400

    adds watchlist

--- watchlist.js
+++ watchlist.js
@@ -1,34 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function renderMovies(movieArray) {
-        let movieHTML = movieArray.map(currentMovie => {
+    let movies = JSON.parse(localStorage.getItem('watchlist'));
+    function renderMovies(movies) {
+        let moviesHTML = movies.map(currentMovie => {
             return `
             <div class="card" style="width: 18rem;">
                 <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
                 <div class="card-body">
                     <h6 class="card-title">${currentMovie.Title} <span class="badge badge-secondary">${currentMovie.Year}</span></h6>
-                    <button href="#" class="btn btn-primary" onclick ="saveToWatchList('${currentMovie.imdbID}')">Add!</button>
+                    <button href="#" class="btn btn-primary" onclick ="removeFromWatchList('${currentMovie.imdbID}')">Remove</button>
                 </div>
             </div>
             `
         });
-        return movieHTML.join('');
+        return moviesHTML.join('');
     }
-    document.getElementById('search-form').addEventListener('submit', e => {
-        e.preventDefault();
-        document.querySelector(".movies-container").innerHTML = renderMovies(movieData);
-    });
+    document.querySelector(".movies-container").innerHTML = renderMovies(movies);
 });
 
-function saveToWatchList(imdbID) {
+function removeFromWatchList(imdbID) {
     let movie = movieData.find(currentMovie => {
         return currentMovie.imdbID == imdbID;
     })
     let watchlistJSON = localStorage.getItem('watchlist');
-    let watchlist = JSON.parse(watchlistJSON);
-    if(watchlist == null) {
-        watchlist = [];
-    }
-    watchlist.push(movie);
+    let watchlist = JSON.parse(watchlistJSON);;
+    watchlist.pop(movie);
     watchlistJSON = JSON.stringify(watchlist);
     localStorage.setItem('watchlist', watchlistJSON);
+    window.location.reload(false);
 }
